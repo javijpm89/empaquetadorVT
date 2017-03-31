11435f2276fc0a5eecc220894d7562473990b62e
javijpm89 <javier.perez.telecor@gmail.com>
2017-03-31 17:43:29 +0200

    Añadidos test

---
+++ src/test/ComprobadorEstructuraAxonTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by javi on 31/03/17.
+ */
+class ComprobadorEstructuraAxonTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compruebaIntegridadHTML() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compruebaIntegridadServicios() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compruebaIntegridadJavauser() {
+    }
+
+}
